87f8478d5d15c73fcb178a287264d7d921e7deea
Gregory Daynes <gregdaynes@gmail.com>
2021-09-08 19:58:51 -0700

    chore: render html for route
    Create a endpoint that captures and responds to all child routes. The
    response is the output of the rendering process.

--- index.js
+++ index.js
@@ -2,21 +2,38 @@ import Fastify from "fastify";
 import Vue from "vue";
 import { createRenderer } from "vue-server-renderer";
 
+const renderer = createRenderer();
+
 const fastify = Fastify({
   logger: true,
 });
 
-const app = new Vue({
-  template: `<div>Hello World</div>`,
-});
-const renderer = createRenderer();
+fastify.get("/*", async (req, reply) => {
+  const app = new Vue({
+    data: {
+      url: req.url,
+    },
+    template: `<div>The visited URL is: {{ url }}</div>`,
+  });
 
-try {
-  const html = await renderer.renderToString(app);
-  fastify.log.info(html);
-} catch (err) {
-  fastify.log.error(err);
-}
+  try {
+    const html = await renderer.renderToString(app);
+
+    reply.type("text/html");
+    reply.send(`
+      <!doctype html>
+      <html lang="en">
+        <head>
+          <title>Hello</title>
+        </head>
+        <body>${html}</body>
+      </html>
+    `);
+  } catch (err) {
+    fastify.log.error(err);
+    throw new Error(err);
+  }
+});
 
 try {
   await fastify.listen(3000);
