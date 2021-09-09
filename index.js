7b533b6572ba40869699b6d380b072c599018a75
Gregory Daynes <gregdaynes@gmail.com>
2021-09-08 19:53:48 -0700

    chore: initial approach to rendering
    Demonstrate the concept of rendering a Vue application to html.
    
    From [Vue SSR Guide](https://ssr.vuejs.org/guide/#rendering-a-vue-instance)

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 import Fastify from "fastify";
+import Vue from "vue";
+import { createRenderer } from "vue-server-renderer";
 
 const fastify = Fastify({
   logger: true,
 });
+
+const app = new Vue({
+  template: `<div>Hello World</div>`,
+});
+const renderer = createRenderer();
+
+try {
+  const html = await renderer.renderToString(app);
+  fastify.log.info(html);
+} catch (err) {
+  fastify.log.error(err);
+}
 
 try {
   await fastify.listen(3000);
